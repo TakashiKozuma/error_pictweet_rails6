2c100f80ee5e0345de2b7f62cc41a69d3f9d4a3a
kozuma_takashi <takashi.kozuma@di-v.co.jp>
2020-09-07 13:47:10 +0900

    エラー仕込み

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -8,26 +8,26 @@ RSpec.describe 'ユーザー新規登録', type: :system do
     it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
       # トップページに移動する
       visit root_path
-      # トップページにサインアップページへ遷移するボタンがある
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
       expect(page).to have_content('新規登録')
       # 新規登録ページへ移動する
       visit new_user_registration_path
       # ユーザー情報を入力する
-      fill_in 'Nickname', with: @user.nickname
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
-      fill_in 'Password confirmation', with: @user.password_confirmation
-      # サインアップボタンを押すとユーザーモデルのカウントが1上がる
+      fill_in 'Nickname', with: "@user.nickname"
+      fill_in 'Email', with: "@user.email"
+      fill_in 'Password', with: "@user.password"
+      fill_in 'Password confirmation', with: "@user.password_confirmation"
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
       expect{
         find('input[name="commit"]').click
       }.to change { User.count }.by(1)
-      # トップページへ遷移する
+      # トップページへ遷移することを確認する
       expect(current_path).to eq root_path
-      # カーソルを合わせるとログアウトボタンが表示される
+      # カーソルを合わせるとログアウトボタンが表示されることを確認する
       expect(
         find(".user_nav").find("span").hover
       ).to have_content('ログアウト')
-      # サインアップページへ遷移するボタンやログインページで遷移するボタンが表示されていない
+      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
       expect(page).to have_no_content('新規登録')
       expect(page).to have_no_content('ログイン')
     end
@@ -36,7 +36,7 @@ RSpec.describe 'ユーザー新規登録', type: :system do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
       # トップページに移動する
       visit root_path
-      # トップページにサインアップページへ遷移するボタンがある
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
       expect(page).to have_content('新規登録')
       # 新規登録ページへ移動する
       visit new_user_registration_path
@@ -45,11 +45,11 @@ RSpec.describe 'ユーザー新規登録', type: :system do
       fill_in 'Email', with: ""
       fill_in 'Password', with: ""
       fill_in 'Password confirmation', with: ""
-      # サインアップボタンを押してもユーザーモデルのカウントは上がらない
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
       expect{
         find('input[name="commit"]').click
       }.to change { User.count }.by(0)
-      # 新規登録ページへ戻される
+      # 新規登録ページへ戻されることを確認する
       expect(current_path).to eq "/users"
     end
   end
@@ -63,7 +63,7 @@ RSpec.describe 'ログイン', type: :system do
     it '保存されているユーザーの情報と合致すればログインができる' do
       # トップページに移動する
       visit root_path
-      # トップページにログインページへ遷移するボタンがある
+      # トップページにログインページへ遷移するボタンがあることを確認する
       expect(page).to have_content('ログイン')
       # ログインページへ遷移する
       visit new_user_session_path
@@ -72,22 +72,22 @@ RSpec.describe 'ログイン', type: :system do
       fill_in 'Password', with: @user.password
       # ログインボタンを押す
       find('input[name="commit"]').click
-      # トップページへ遷移する
+      # トップページへ遷移することを確認する
       expect(current_path).to eq root_path
-      # カーソルを合わせるとログアウトボタンが表示される
+      # カーソルを合わせるとログアウトボタンが表示されることを確認する
       expect(
         find(".user_nav").find("span").hover
       ).to have_content('ログアウト')
-      # サインアップページへ遷移するボタンやログインページで遷移するボタンが表示されていない
+      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
       expect(page).to have_no_content('新規登録')
       expect(page).to have_no_content('ログイン')
     end
   end
   context 'ログインができないとき' do
     it '保存されているユーザーの情報と合致しないとログインができない' do
-      #トップページに移動する
+      # トップページに移動する
       visit root_path
-      # トップページにログインページへ遷移するボタンがある
+      # トップページにログインページへ遷移するボタンがあることを確認する
       expect(page).to have_content('ログイン')
       # ログインページへ遷移する
       visit new_user_session_path
@@ -96,7 +96,7 @@ RSpec.describe 'ログイン', type: :system do
       fill_in 'Password', with: ""
       # ログインボタンを押す
       find('input[name="commit"]').click
-      # 新規登録ページへ戻される
+      # ログインページへ戻されることを確認する
       expect(current_path).to eq new_user_session_path
     end
   end
